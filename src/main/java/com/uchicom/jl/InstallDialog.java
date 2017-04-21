7546cec95208698433818918e0cf0fcaebfc7dc8
Shigeki Uchiyama <shigeki@wk2>
2017-04-21 10:09:30 +0900

    ダイアログの追加と、rssrのプロジェクト参照追加

---
+++ src/main/java/com/uchicom/jl/InstallDialog.java
@@ -0,0 +1,24 @@
+// (c) 2017 uchicom
+package com.uchicom.jl;
+
+import javax.swing.JDialog;
+
+/**
+ * @author uchicom: Shigeki Uchiyama
+ *
+ */
+public class InstallDialog extends JDialog {
+	public InstallDialog() {
+		initComponents();
+	}
+
+	/**
+	 *
+	 */
+	private void initComponents() {
+		//メニュー名前(menu.editor.ホゲ)(親メニュー.子メニューで階層を指定できます)
+		//起動タイプ(Runtime.exec(別プロセス),同一プロセス,)
+		//
+
+	}
+}
--- src/main/java/com/uchicom/jl/Launcher.java
+++ src/main/java/com/uchicom/jl/Launcher.java
@@ -2,13 +2,20 @@
 package com.uchicom.jl;
 
 import java.awt.AWTException;
+import java.awt.Menu;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
 import java.awt.SystemTray;
 import java.awt.TrayIcon;
 import java.awt.image.BufferedImage;
+import java.io.File;
 
 import javax.imageio.ImageIO;
 
+import com.uchicom.rssr.RssrFrame;
+
 /**
+ * ActionListenerを自作するか、MainArgsで指定するか、main
  * @author uchicom: Shigeki Uchiyama
  *
  */
@@ -21,19 +28,37 @@ public class Launcher {
 			// タスクトレイ
 			BufferedImage image = new BufferedImage(16, 16,
 					BufferedImage.TYPE_INT_BGR);
-
+			//背景はプログラムで変更すると良い
 			try {
 				image = ImageIO.read(Thread.currentThread()
 						.getContextClassLoader()
-						.getResource("img/tray_icon.png"));
+						.getResource("com/uchicom/jl/image/jl.png"));
 			} catch (Exception e) {
+				e.printStackTrace();
 			}
+			PopupMenu popup = new PopupMenu();
 
-			TrayIcon trayIcon = new TrayIcon(image, "test");
+			Menu menu = new Menu("editor");
+			popup.add(menu);
+			MenuItem menuItem = new MenuItem("rssr");
+			menuItem.addActionListener((e)->{
+				RssrFrame frame = new RssrFrame(new File("C:\\Users\\shigeki\\Dropbox\\uchicom\\software\\rssr\\conf\\rssr.properties"));
+				frame.setVisible(true);
+			});
+			menu.add(menuItem);
+			menu = new Menu("設定");menuItem = new MenuItem("インストール");
+			menuItem.addActionListener((e)->{
+				InstallDialog dialog = new InstallDialog();
+				dialog.setModal(true);
+				dialog.setVisible(true);
+			});
+			menu.add(menuItem);
+			popup.add(menu);
+			TrayIcon trayIcon = new TrayIcon(image, "jl");
+			trayIcon.setPopupMenu(popup);
 			try {
 				tray.add(trayIcon);
 			} catch (AWTException e) {
-				// TODO 自動生成された catch ブロック
 				e.printStackTrace();
 			}
 
